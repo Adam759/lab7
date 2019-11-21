4e5096599aa1e8db2eaf6a86c37833de4eae6c90
Adam759 <adam759@comcast.net>
2019-11-21 01:55:48 -0600

    new js push, should be it

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
 
-
-
-
-
-
-
+  document.addEventListener('click', function (event) {
+    if (!event.target.matches('#entrybutton')) return; //Listen for all clicks, return if not the entrybutton
+    var textbox = document.getElementById('entryinput').value; //Set var textbox to the value of entryinput
+    alert('Adam Patton: ' + textbox); //Alert with the textbox input value
+    document.getElementById("textoutput").innerHTML = textbox;
+  });
+}
 
 window.addEventListener('load', init);
