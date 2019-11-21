9cc19826ae1961bfcfe80b42e3c9c6a18055ff7a
Adam759 <adam759@comcast.net>
2019-11-21 06:11:36 -0600

    Js tweak

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 function init(){
 //add your javascrip between these two lines of code
 
-  document.addEventListener('click', function (event) {
-    if (!event.target.matches('#entrybutton')) return; //Listen for all clicks, return if not the entrybutton
-    var textbox = document.getElementById('entryinput').value; //Set var textbox to the value of entryinput
-    alert('Adam Patton: ' + textbox); //Alert with the textbox input value
-    document.getElementById("textoutput").innerHTML = textbox;
+  document.addEventListener('click', function (event) { //Listen for all clicks
+    if (event.target.matches('#entrybutton')) { //Filter out entrybutton click
+      var textbox = document.getElementById('entryinput').value; //Set var textbox to the value of entryinput
+      alert('Adam Patton: ' + textbox); //Alert with the textbox input value
+      document.getElementById("textoutput").innerHTML = textbox;
+  }
   });
 }
 
 window.addEventListener('load', init);
+
+//I configured the listener as such to allow for simply adding a new if statement to capture a click on a different element, if desired
